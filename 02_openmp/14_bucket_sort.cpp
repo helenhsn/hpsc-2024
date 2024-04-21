fd628dbb0a1b4da46fc94ddad27bf65994a6e51c
helenhsn <mimihassan34@gmail.com>
2024-04-21 17:35:59 +0900

    HW 22/04: further tests

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -6,24 +6,19 @@
 
 void bucket_sort_par(int n, int range, int THRESHOLD)
 {
-  double start;
-  start = omp_get_wtime();
 
   omp_set_num_threads(std::min(range, THRESHOLD));
-  printf("TASK 0 = %f\n", omp_get_wtime() - start);
 
 
   std::vector<int> key(n);
   
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
-    // printf("%d ",key[i]);
   }
   printf("\n");
 
   std::vector<int> bucket(range,0); 
 
-  start = omp_get_wtime();
   #pragma omp parallel for shared(bucket) schedule(guided)
   for (int i=0; i<n; i++)
   {
@@ -32,90 +27,55 @@ void bucket_sort_par(int n, int range, int THRESHOLD)
     bucket[number]++;
   }
 
-  printf("TASK 1 = %f\n", omp_get_wtime() - start);
 
-
-  start = omp_get_wtime();
   
-  std::vector<int> temp(range,0);
   std::vector<int> offset(range,0);
 
-  #pragma omp parallel for
   for (int i=1; i<range; i++)
-  {
-    offset[i] = bucket[i-1];
-  }
+    offset[i] = offset[i-1] + bucket[i-1];
+
   
-  #pragma omp parallel
-  for (int j=1; j<range; j<<=1) 
-  {
-    #pragma omp for
-    for (int k=0; k<range; k++)
-    {
-      temp[k] = offset[k];
-    }
-
-    #pragma omp for
-    for (int i=j; i<range; i++)
-    {
-      offset[i] += temp[i-j]; 
-    }
-  }
-
-  printf("TASK 2= %f\n", omp_get_wtime() - start);
-
-  start = omp_get_wtime();
+  double start = omp_get_wtime();
   #pragma omp parallel for schedule(static)
   for (int i=0; i<range; i++) {
+    
     int nb_occurences = bucket[i]; 
     int off = offset[i];
+
     for (int k=0; k<nb_occurences; k++) {
       key[k+off] = i;
     }
   }
-  printf("TASK 3 = %f\n", omp_get_wtime() - start);
+  printf("TASK  = %f\n", omp_get_wtime() - start);
 
-
-  // for (int i=0; i<n; i++) {
-  //   printf("%d ",key[i]);
-  // }
 }
 
 void bucket_sort(int n, int range)
 {
-  double start;
   std::vector<int> key(n);
   
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
-    // printf("%d ",key[i]);
   }
   printf("\n");
   
-  start = omp_get_wtime();
 
   std::vector<int> bucket(range,0); 
 
   for (int i=0; i<n; i++)
     bucket[key[i]]++;
 
-  printf("TASK 1 = %f\n", omp_get_wtime() - start);
   
   std::vector<int> temp(range,0);
   std::vector<int> offset(range, 0);
 
-
-  start = omp_get_wtime();
 
   for (int i=1; i<range; i++) 
   {
     offset[i] = offset[i-1] + bucket[i-1];
   }
 
-  printf("TASK 2 = %f\n", omp_get_wtime() - start);
-
-
-  start = omp_get_wtime();
+  double start = omp_get_wtime();
 
   for (int i=0; i<range; i++) {
     int j = offset[i];
@@ -123,11 +83,8 @@ void bucket_sort(int n, int range)
       key[j++] = i;
     }
   }
-  printf("TASK 3 = %f\n", omp_get_wtime() - start);
+  printf("TASK  = %f\n", omp_get_wtime() - start);
 
-  // for (int i=0; i<n; i++) {
-  //   printf("%d ",key[i]);
-  // }
 }
 
 int main() {
