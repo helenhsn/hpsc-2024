f666f364c497b7593b295589c255a5359fcfc834
helenhsn <mimihassan34@gmail.com>
2024-04-21 17:21:01 +0900

    HW 22/04: fixed algo

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -6,8 +6,13 @@
 
 void bucket_sort_par(int n, int range, int THRESHOLD)
 {
+  double start;
+  start = omp_get_wtime();
+
   omp_set_num_threads(std::min(range, THRESHOLD));
-  // printf("n threads = %i", omp_get_max_threads());
+  printf("TASK 0 = %f\n", omp_get_wtime() - start);
+
+
   std::vector<int> key(n);
   
   for (int i=0; i<n; i++) {
@@ -16,24 +21,31 @@ void bucket_sort_par(int n, int range, int THRESHOLD)
   }
   printf("\n");
 
-  int bucket[range] = {0}; 
+  std::vector<int> bucket(range,0); 
 
-  #pragma omp parallel for shared(bucket)
+  start = omp_get_wtime();
+  #pragma omp parallel for shared(bucket) schedule(guided)
   for (int i=0; i<n; i++)
   {
+    int number = key[i];
     #pragma omp atomic update
-    bucket[key[i]]++;
+    bucket[number]++;
   }
 
-  int temp[range] ={0};
-  int offset[range] = {0};
+  printf("TASK 1 = %f\n", omp_get_wtime() - start);
+
+
+  start = omp_get_wtime();
+  
+  std::vector<int> temp(range,0);
+  std::vector<int> offset(range,0);
 
   #pragma omp parallel for
   for (int i=1; i<range; i++)
   {
     offset[i] = bucket[i-1];
   }
-
+  
   #pragma omp parallel
   for (int j=1; j<range; j<<=1) 
   {
@@ -50,14 +62,19 @@ void bucket_sort_par(int n, int range, int THRESHOLD)
     }
   }
 
-  #pragma omp parallel for
-  for (int i=0; i<range; i++) {
-    int start = (i==0) ? 0 : offset[i-1];
+  printf("TASK 2= %f\n", omp_get_wtime() - start);
 
-    for (int k=start; k<offset[i]; k++) {
-      key[k] = i;
+  start = omp_get_wtime();
+  #pragma omp parallel for schedule(static)
+  for (int i=0; i<range; i++) {
+    int nb_occurences = bucket[i]; 
+    int off = offset[i];
+    for (int k=0; k<nb_occurences; k++) {
+      key[k+off] = i;
     }
   }
+  printf("TASK 3 = %f\n", omp_get_wtime() - start);
+
 
   // for (int i=0; i<n; i++) {
   //   printf("%d ",key[i]);
@@ -66,7 +83,7 @@ void bucket_sort_par(int n, int range, int THRESHOLD)
 
 void bucket_sort(int n, int range)
 {
-  
+  double start;
   std::vector<int> key(n);
   
   for (int i=0; i<n; i++) {
@@ -74,21 +91,31 @@ void bucket_sort(int n, int range)
     // printf("%d ",key[i]);
   }
   printf("\n");
+  
+  start = omp_get_wtime();
 
   std::vector<int> bucket(range,0); 
 
   for (int i=0; i<n; i++)
     bucket[key[i]]++;
 
+  printf("TASK 1 = %f\n", omp_get_wtime() - start);
+  
   std::vector<int> temp(range,0);
   std::vector<int> offset(range, 0);
 
+
+  start = omp_get_wtime();
 
   for (int i=1; i<range; i++) 
   {
     offset[i] = offset[i-1] + bucket[i-1];
   }
 
+  printf("TASK 2 = %f\n", omp_get_wtime() - start);
+
+
+  start = omp_get_wtime();
 
   for (int i=0; i<range; i++) {
     int j = offset[i];
@@ -96,6 +123,7 @@ void bucket_sort(int n, int range)
       key[j++] = i;
     }
   }
+  printf("TASK 3 = %f\n", omp_get_wtime() - start);
 
   // for (int i=0; i<n; i++) {
   //   printf("%d ",key[i]);
@@ -104,7 +132,7 @@ void bucket_sort(int n, int range)
 
 int main() {
   std::vector<int> n_values = {50, 500, 1000, 10000, 1000000, 100000000, 500000000};
-  std::vector<int> range_values = {5, 50, 100, 1000, 50000, 100000, 500000};
+  std::vector<int> range_values = {6, 50, 100, 1000, 50000, 100000, 500000};
   std::vector<int> thresholds = {32, 32, 32, 32, 32, 256, 1024};
 
   for (int i=0; i<n_values.size(); i++)
