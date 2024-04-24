676dcb7e8d51a04ef32fb472aa333d4decad7a31
helenhsn <mimihassan34@gmail.com>
2024-04-24 16:31:04 +0900

    HW 04/25: more cleaning

--- 04_simd/11_nbody.cpp
+++ 04_simd/11_nbody.cpp
@@ -19,7 +19,7 @@ int main() {
   float a[N];
 
   for(int i=0; i<N; i++) {
-    printf("\n");
+
     for(int j=0; j<N; j++) {
       if(i != j) {
         float rx = x[i] - x[j];
@@ -31,40 +31,43 @@ int main() {
       }
     }
 
-    __m512 minusx_vec = _mm512_sub_ps(_mm512_set1_ps(x[i]), _mm512_load_ps(x)); // minusx_vec = (x[i]-x[j]) for all j=0..N-1
+    // HOMEWORK: vectorization of the for loop above for parameter j.
+    {
+      __m512 minusx_vec = _mm512_sub_ps(_mm512_set1_ps(x[i]), _mm512_load_ps(x)); // minusx_vec = (x[i]-x[j]) for all j=0..N-1
+      
+      __m512 minusy_vec = _mm512_sub_ps(_mm512_set1_ps(y[i]), _mm512_load_ps(y)); // minusy_vec = (y[i]-y[j]) for all j=0..N-1
     
-    __m512 minusy_vec = _mm512_sub_ps(_mm512_set1_ps(y[i]), _mm512_load_ps(y)); // minusy_vec = (y[i]-y[j]) for all j=0..N-1
-  
-    // if condition
-    __m512 minusij_vec = _mm512_sub_ps(_mm512_set1_ps(i), _mm512_load_ps(range)); // minusij_vec = (i - j) for all j=0..N-1
-    __m512 zeros = _mm512_set1_ps(0);
-    __mmask16 mask_if = _mm512_cmp_ps_mask(minusij_vec, zeros, _MM_CMPINT_NE);
+      // if condition
+      __m512 minusij_vec = _mm512_sub_ps(_mm512_set1_ps(i), _mm512_load_ps(range)); // minusij_vec = (i - j) for all j=0..N-1
+      __m512 zeros = _mm512_set1_ps(0);
+      __mmask16 mask_if = _mm512_cmp_ps_mask(minusij_vec, zeros, _MM_CMPINT_NE);
 
-    __m512 rxvec = _mm512_mask_blend_ps(mask_if, zeros, minusx_vec);
-    __m512 rxsqvec = _mm512_mul_ps(rxvec, rxvec);
+      __m512 rxvec = _mm512_mask_blend_ps(mask_if, zeros, minusx_vec);
+      __m512 rxsqvec = _mm512_mul_ps(rxvec, rxvec);
 
-    __m512 ryvec =  _mm512_mask_blend_ps(mask_if, zeros, minusy_vec);
-    __m512 rysqvec = _mm512_mul_ps(ryvec, ryvec);
+      __m512 ryvec =  _mm512_mask_blend_ps(mask_if, zeros, minusy_vec);
+      __m512 rysqvec = _mm512_mul_ps(ryvec, ryvec);
 
-    __m512 rvec = _mm512_add_ps(rxsqvec, rysqvec);
+      __m512 rvec = _mm512_add_ps(rxsqvec, rysqvec);
 
 
-    __mmask16 mask_div = _mm512_cmp_ps_mask(rvec, zeros, _MM_CMPINT_EQ);
-    rvec = _mm512_mask_blend_ps(mask_div, rvec, _mm512_set1_ps(1));
-    rvec = _mm512_rsqrt14_ps(rvec); //rvec contains 1/r now
-    rvec = _mm512_mul_ps(rvec, _mm512_mul_ps(rvec, rvec)); //rvec contains now 1/(r*r*r)
+      __mmask16 mask_div = _mm512_cmp_ps_mask(rvec, zeros, _MM_CMPINT_EQ);
+      rvec = _mm512_mask_blend_ps(mask_div, rvec, _mm512_set1_ps(1));
+      rvec = _mm512_rsqrt14_ps(rvec); //rvec contains 1/r now
+      rvec = _mm512_mul_ps(rvec, _mm512_mul_ps(rvec, rvec)); //rvec contains now 1/(r*r*r)
 
-    rvec = _mm512_mask_blend_ps(mask_div, rvec, zeros); // rvec contains either 1/(r*r*r) or 0.0 (if r=0)
+      rvec = _mm512_mask_blend_ps(mask_div, rvec, zeros); // rvec contains either 1/(r*r*r) or 0.0 (if r=0)
 
-    __m512 mvec = _mm512_load_ps(m);
-    __m512 fxvec_temp = _mm512_mask_sub_ps(zeros, mask_if, zeros, _mm512_mul_ps(_mm512_mul_ps(rxvec, rvec), mvec));
-    __m512 fyvec_temp = _mm512_mask_sub_ps(zeros, mask_if, zeros, _mm512_mul_ps(_mm512_mul_ps(ryvec, rvec), mvec));
+      __m512 mvec = _mm512_load_ps(m);
+      __m512 fxvec_temp = _mm512_mask_sub_ps(zeros, mask_if, zeros, _mm512_mul_ps(_mm512_mul_ps(rxvec, rvec), mvec));
+      __m512 fyvec_temp = _mm512_mask_sub_ps(zeros, mask_if, zeros, _mm512_mul_ps(_mm512_mul_ps(ryvec, rvec), mvec));
 
 
-    fxSIMD[i] = _mm512_reduce_add_ps(fxvec_temp);
-    fySIMD[i] = _mm512_reduce_add_ps(fyvec_temp);
+      fxSIMD[i] = _mm512_reduce_add_ps(fxvec_temp);
+      fySIMD[i] = _mm512_reduce_add_ps(fyvec_temp);
+    }
 
-    printf("COMPARISON for value %d ==> NON SIMD %g %g || SIMD = %g %g \n",i,fx[i],fy[i], fxSIMD[i],fySIMD[i]);
+    printf("COMPARISON for value %d ==> NON SIMD %g %g || SIMD = %g %g \n\n",i,fx[i],fy[i], fxSIMD[i],fySIMD[i]);
 
 }
 }
